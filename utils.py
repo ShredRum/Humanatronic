70fe6b0e71b5169ca8d2bcfa3aa7641db879932b
Allnorm <ruallnorm@gmail.com>
2023-11-18 20:37:24 +0300

    Updated to version 1.2.3 1. Improvements in the area of prompt

--- utils.py
+++ utils.py
@@ -184,7 +184,7 @@ def html_fix(text):
 
 
 def current_time_info(config):
-    current_time = time.strftime("%d.%m.%Y, %H:%M", time.gmtime(int(time.time()) + config.timezone * 3600))
+    current_time = time.strftime("%d.%m.%Y, %a, %H:%M", time.gmtime(int(time.time()) + config.timezone * 3600))
     return f"Current time: {current_time}"
 
 
