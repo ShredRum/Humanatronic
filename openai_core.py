d403293bb4363a605d1f6f36aa1a1e37ff348ff4
Allnorm <ruallnorm@gmail.com>
2023-11-24 09:26:23 +0300

    Updated to version 2.1.4 1. Fixed a small bug with time reminder

--- openai_core.py
+++ openai_core.py
@@ -50,7 +50,7 @@ class Dialog:
         if random.randint(1, 50) == 1:
             prompt += f"{self.config.prompts.prefill} "
             logging.info(f"Prompt reminded for dialogue in chat {chat_name}")
-        if random.randint(1, 30) == 1 or "врем" in message.text or "час" in message.text:
+        if random.randint(1, 30) == 1 or "врем" in message.text.lower() or "час" in message.text.lower():
             prompt += f"{utils.current_time_info(self.config)} "
             logging.info(f"Time updated for dialogue in chat {chat_name}")
         prompt += f"{utils.username_parser(message)}: {message.text}"
